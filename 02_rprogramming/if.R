50f87c4584f1621d005d0e2e38ee3508ab00842c
jjkoretoff <jjkoretoff@gmail.com>
2016-04-22 20:34:59 -0400

    study time

---
+++ 02_rprogramming/if.R
@@ -0,0 +1,26 @@
+if(condition) {
+  # do something
+} else {
+  # do something else
+}
+if (condition) {
+  # do something
+} else if (condition2) {
+  # do something
+} else {
+  # do something different
+}
+
+# EXAMPLE
+if(x > 3) {
+  y <- 10
+} else {
+  y <- 0
+}
+
+# EXAMPLE 
+y <- if(x > 3) {
+  10
+} else {
+  0
+}
---
+++ 02_rprogramming/repeat-next-break.R
@@ -0,0 +1,12 @@
+x0 <- 1
+tol <- 1e-8
+
+repeat {
+  x1 <- computeEstimate()
+  
+  if(abs(x1 - x0) < tol) {
+    break
+  } else {
+    x0 <- x1
+  }
+}
