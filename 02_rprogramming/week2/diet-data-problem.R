fa0815a533ff527f8c1a5060e36df514990cac69
jjkoretoff <jjkoretoff@gmail.com>
2016-04-25 15:18:50 -0400

    updated

--- 02_rprogramming/week2/diet-data-problem.R
+++ 02_rprogramming/week2/diet-data-problem.R
@@ -49,5 +49,80 @@ andy_loss
 files <- list.files("diet_data")
 files
 
+#subset files bc we know they are in alphabetical order
+files[1]
+files[2]
+files[3:5]
 
+#look at John's data
+#head(read.csv(files[3])) returns an error so we edit the code
 
+#edited
+files_full <- list.files("diet_data", full.names=TRUE)
+files_full
+
+#now try looking at John again
+head(read.csv(files_full[3]))
+
+#now we make a massive frame with rbind and a loop
+andy_david <- rbind(andy, read.csv(files_full[2]))
+
+#check the code
+head(andy_david)
+tail(andy_david)
+
+#observe the 25th day for Andy and David
+day_25 <- andy_david[which(andy_david$Day == 25), ]
+day_25
+
+#now we build the loop - test - we will run this through all the data
+for (i in 1:5) {print(i)}
+
+#apply the loop to all our files - error - can't rbind() something that DNE
+#for (i in 1:5) {
+#      dat <- rbind(dat, read.csv(files_full[i]))
+#}
+
+#therefore, we create an empty data frame
+dat <- data.frame()
+for (i in 1:5) {
+      dat <- rbind(dat, read.csv(files_full[i]))
+}
+str(dat)
+#note, dat cannot go inside the loop bc we will only be returned the last file
+
+#now we find the median weight of the entire "dat" frame
+median(dat$Weight)
+#woah! we had an na...... have to fix that
+
+#problem. solved.
+median(dat$Weight, na.rm = TRUE)
+
+#find the mean weight of day 30
+dat_30 <- dat[which(dat[, "Day"] == 30),]
+dat_30
+median(dat_30$Weight, na.rm = TRUE)
+
+#build a function that will return a median weight on a given day
+#weightmedian <- function(directory, day) { # content of the function }
+
+#need to put the data in one frame - use list.file() and rbind()
+#run loop through
+weightmedian <- function(directory, day)  {
+      files_list <- list.files(directory, full.names=TRUE)   #creates a list of files
+      dat <- data.frame()                             #creates an empty data frame
+      for (i in 1:5) {                                
+            #loops through the files, rbinding them together 
+            dat <- rbind(dat, read.csv(files_list[i]))
+      }
+      dat_subset <- dat[which(dat[, "Day"] == day),]  #subsets the rows that match the 'day' argument
+      median(dat_subset[, "Weight"], na.rm=TRUE)      #identifies the median weight 
+      #while stripping out the NAs
+}
+
+#test for above
+weightmedian(directory = "diet_data", day = 20)
+weightmedian("diet_data", 4)
+weightmedian("diet_data", 17)
+
+#REVIEW THE LAST PORTION OF THIS EXAMPLE FOR MORE SOPHISTICATED APPROACH
